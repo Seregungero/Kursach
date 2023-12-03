64f24deb8bb54f85d85aa7bd587a9a54dbc3aada
Leonid-poc <legeorg2004@gmail.com>
2023-12-03 11:37:39 +0500

    Add routing to cart of user

--- client/src/components/Main.jsx
+++ client/src/components/Main.jsx
@@ -4,15 +4,17 @@ import SignUp from './SignUp';
 import SignIn from './SignIn';
 import Dashboard from './Dashboard';
 import ViewProfile from './ViewProfile';
+import CartUser from './CartUser';
 
 const Main = ({ admins }) => {
 	return (
 		<main className="Main">
 			<Routes>
 				<Route path='/' element={<Dashboard />} />
+				<Route path='/cart' element={<CartUser />}/>
+				<Route path='/userinfo' element={<ViewProfile />} />
 				<Route path='/register' element={<SignUp admins={admins} />} />
 				<Route path='/login' element={<SignIn admins={admins} />} />
-				<Route path='/userinfo' element={<ViewProfile admins={admins} />} />
 			</Routes>
 		</main>
 	)
